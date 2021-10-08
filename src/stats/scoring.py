7e664f0c535862568348fb3421c123d715828d85
Ruosi Wang <wang.ruosi@outlook.com>
2021-10-08 09:41:18 -0400

    Updating stats testing

--- src/stats/scoring.py
+++ src/stats/scoring.py
@@ -3,8 +3,9 @@ import numpy as np
 from scipy.stats import wilcoxon
 from mne.stats import fdr_correction
 
+from ..configs import T_RANGE
 
-def get_sig_timepoints(xs, times, alpha=.05, t_range=(.1, .5), alternative='two-sided'):
+def get_sig_timepoints(xs, times, alpha=.05, t_range=T_RANGE, alternative='two-sided'):
     if t_range is not None:
         tslice = slice(np.where(times == t_range[0])[0][0],
                        np.where(times == t_range[1])[0][0])
