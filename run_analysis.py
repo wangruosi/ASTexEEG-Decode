bc4cd404f7a6d56aa673ff86f4df3f837904cada
Ruosi Wang <wang.ruosi@outlook.com>
2021-10-08 23:56:53 -0400

    Update run analysis

--- run_analysis.py
+++ run_analysis.py
@@ -3,20 +3,20 @@ from src import run
 
 if __name__ == '__main__':
 
-    # Count the number of trials that survive artifact rejection
+    # # Count the number of trials that survive artifact rejection
     run.run_qa_n_trials()
     
-    # Decode animacy (animal vs. object) and size (big vs. small)
+    # # Decode animacy (animal vs. object) and size (big vs. small)
     run.run_category_decoding(cross=False)
     run.run_category_decoding(cross=True)
     
-    # Estimate the onset/peak latencies via bootstrapping
+    # # Estimate the onset/peak latencies via bootstrapping
     run.run_get_latency()
 
-    # Make EEG representational dissimilarity matrices
+    # # Make EEG representational dissimilarity matrices
     run.run_eeg_rdm()
     
-    # Decode size separately for animals and objects
+    # # Decode size separately for animals and objects
     run.run_tripartite_size_decoding()
     
     # Decode all pairs of objects
